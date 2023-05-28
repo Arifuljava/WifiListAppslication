0181f84bb5d7157046cfb31406762c5d64d1de12
Ariful0007 <arifulpub143@gmail.com>
2023-05-28 16:52:46 +0600

    llll

--- WifiListAppslication/ViewController.swift
+++ WifiListAppslication/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import UIKit
+import NetworkExtension
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var wifiNetworks: [NEHotspotNetwork] = []
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+               
+               fetchWifiNetworks() // Fetch available Wi-Fi networks
     }
 
 
-}
+    func fetchWifiNetworks() {
+        print("gggg")
+           NEHotspotHelper.register(options: nil, queue: DispatchQueue.main) { [weak self] (command) in
+               guard let strongSelf = self else { return }
+               
+               if let networks = command.networkList {
+                   strongSelf.wifiNetworks = networks
+                   DispatchQueue.main.async {
+                       strongSelf.tableView.reloadData() // Reload the table view data
+                   }
+               }
+           }
+        print(wifiNetworks.count)
+       }
+       
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return wifiNetworks.count
+           print(wifiNetworks.count)
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "celll", for: indexPath)
+           let network = wifiNetworks[indexPath.row]
+           cell.textLabel?.text = network.ssid
+           
+           return cell
+       }
+   }
 
